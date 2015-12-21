6bbe46135f1e8aec3ca8d8f7ab2a076d6dfc0cb7
Ali Afroozeh <afroozeh@gmail.com>
2015-12-21 03:11:38 +0100

    Converted to scalatest

--- src/test/scala/IguanaExamples.scala
+++ src/test/scala/IguanaExamples.scala
@@ -1,27 +1,21 @@
 import iguana.parsetrees.sppf.SPPFVisualization
 import iguana.parsetrees.term.TermVisualization
-import org.iguana.grammar.symbol.{Start, Nonterminal}
+import org.iguana.grammar.symbol.Nonterminal
 import iguana.utils.input.Input
 import org.iguana.grammar.iggy.IggyParser
 import org.iguana.parser.Iguana
+import org.scalatest.FunSuite
 
-object IguanaExamples extends App {
+class IguanaExamples extends FunSuite {
 
-  parseSimple
-  parseXML
-  parseExpression
-  parseIndentation
-
-//  val indentationGrammar = IggyParser.getGrammar(Input.fromPath("src/resources/grammars/IndentationSensitive.iggy"))
-
-//  val expressionGrammar = IggyParser.getGrammar(Input.fromPath("src/resources/grammars/Expression.iggy"))
-
-  def parseSimple(): Unit = {
+  test("SimpleGrammar") {
     val grammar = IggyParser.getGrammar(Input.fromPath("src/resources/grammars/Simple.iggy"))
     val input = Input.fromString("aaa")
     val start = Nonterminal.withName("A")
 
     val result = Iguana.parse(input, grammar, start)
+
+    assert(result.isParseSuccess)
 
     if (result.isParseSuccess) {
       SPPFVisualization.generate(result.asParseSuccess().getSPPFNode, "graphs", "simple_sppf")
@@ -29,30 +23,41 @@ object IguanaExamples extends App {
     }
   }
 
-  def parseXML {
+  test("XML Parser") {
     val grammar = IggyParser.getGrammar(Input.fromPath("src/resources/grammars/XML.iggy"))
     val input = """|<note>
-                   |    <from>Bob</from>
-                   |    <to>Alice</to>
-                   |</note>""".stripMargin
+                  |    <from>Bob</from>
+                  |    <to>Alice</to>
+                  |</note>""".stripMargin
 
     val start = grammar.getStartSymbol(Nonterminal.withName("Start"))
 
     val result = Iguana.parse(input, grammar, start)
+
+    assert(result.isParseSuccess)
 
     if (result.isParseSuccess) {
       SPPFVisualization.generate(result.asParseSuccess().getSPPFNode, "graphs", "xml_sppf")
       TermVisualization.generate(result.asParseSuccess().getTree(), "graphs", "xml_terms")
       TermVisualization.generateWithoutLayout(result.asParseSuccess().getTree(), "graphs", "xml_terms_no_layout")
     }
+
+    val input2 = """|<note>
+                    |    <from>Bob</to>
+                    |    <to>Alice</from>
+                    |</note>""".stripMargin
+
+    assert(Iguana.parse(input2, grammar, start).isParseError)
   }
 
-  def parseExpression {
+  test("Expressions") {
     val grammar = IggyParser.getGrammar(Input.fromPath("src/resources/grammars/Expression.iggy"))
     val input = "1 + - if 0 then 2 else 3 + 4"
     val start = grammar.getStartSymbol("start")
 
     val result = Iguana.parse(input, grammar, start)
+
+    assert(result.isParseSuccess)
 
     if (result.isParseSuccess) {
       SPPFVisualization.generate(result.asParseSuccess().getSPPFNode, "graphs", "expr_sppf")
@@ -62,7 +67,7 @@ object IguanaExamples extends App {
   }
 
 
-  def parseIndentation {
+  test("Indentation") {
     val grammar = IggyParser.getGrammar(Input.fromPath("src/resources/grammars/IndentationSensitive.iggy"))
     val input = """|let x = 1
                    |    y = let z = 2
@@ -75,12 +80,23 @@ object IguanaExamples extends App {
 
     val result = Iguana.parse(input, grammar, start)
 
+    assert(result.isParseSuccess)
+
     if (result.isParseSuccess) {
       SPPFVisualization.generate(result.asParseSuccess().getSPPFNode, "graphs", "indentation_sppf")
       TermVisualization.generate(result.asParseSuccess().getTree(), "graphs", "indentation_terms")
       TermVisualization.generateWithoutLayout(result.asParseSuccess().getTree(), "graphs", "indentation_terms_no_layout")
     }
+
+    val input2 = """let x = 1
+                   |    y = let z = 2
+                   |            w = 3
+                   |      in 2
+                   |   + 3
+                   |in 1 + 2 + 3"""
+
+    assert(Iguana.parse(input2, grammar, start).isParseError)
   }
 
-    implicit def toInput(s: String): Input = Input.fromString(s)
+  implicit def toInput(s: String): Input = Input.fromString(s)
 }
